36b3510cce183a02b5a3764019ad2d9c86c22045
aubinfranck-hub <aubinfranck@gmail.com>
2026-09-22 20:17:54 +0000

    Add QR scanner for technician tablet pairing

--- android/diagassist-technician/app/build.gradle.kts
+++ android/diagassist-technician/app/build.gradle.kts
@@ -23,4 +23,5 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-service:2.8.7")
   implementation("com.squareup.okhttp3:okhttp:4.12.0")
   implementation("io.github.webrtc-sdk:android:144.7559.04")
+  implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
