882d4f5ddab2dc5862ec2f05b38d11accc9915f8
aubinfranck-hub <aubinfranck@gmail.com>
2026-09-22 10:13:09 +0000

    Android: version 2.0.1 avec correction appairage

--- android/diagassist-technician/app/build.gradle.kts
+++ android/diagassist-technician/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
     applicationId="com.diagassist.technician"
     minSdk=26
     targetSdk=35
-    versionCode=1
-    versionName="2.0.0"
+    versionCode=2
+    versionName="2.0.1"
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
